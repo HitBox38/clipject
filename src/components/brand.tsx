f8a9617598a7a00a14914da1ecded8dc9de71a11
Tomer Norman <spidernor38@gmail.com>
2026-09-23 15:36:39 +0300

    Use current ClipJect icon in popup and library

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -2,16 +2,7 @@ export function Brand() {
   return (
     <div className="brand">
       <span className="brand-symbol" aria-hidden="true">
-        <svg
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="1.8"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M9 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-3M9 3h6v4H9zM14 12h7m-3-3 3 3-3 3" />
-        </svg>
+        <img src="/icons/clipject.svg" alt="" width="28" height="28" />
       </span>
       <span>ClipJect</span>
     </div>
